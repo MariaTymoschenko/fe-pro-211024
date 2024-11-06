fa39e06be4a18b821643d0436cb85b2167a88a03
MariaTymoschenko <tymoschenko.maria@gmail.com>
2024-11-06 23:59:17 +0200

    hw01

---
+++ 01/script.js
@@ -0,0 +1,18 @@
+let userNickname = 'Maria';
+let userScore = 11;
+let age = 28;
+console.log(age);
+age += 5;
+console.log(age);
+
+let isStudent = true;
+console.log(isStudent);
+isStudent = false;
+console.log(isStudent);
+
+let item = 10;
+console.log(item);
+item = 'ten';
+console.log(item);
+item = true;
+console.log(item);

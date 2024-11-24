bfc36d03817f5f60685974d22560bf9ce342a5ac
MariaTymoschenko <tymoschenko.maria@gmail.com>
2024-11-24 21:36:06 +0200

    hw 07

--- 07/07.js
+++ 07/07.js
@@ -1,42 +1,47 @@
 // spread
-// function funct(array1, array2) {
-//     return [... array1, ... array2];
-// }
-// let array1 = [1, 2];
-// let array2 = [7, 8];
-// let arrays = funct(array1, array2);
-// console.log(arrays);
+function funct(array1, array2) {
+    return [... array1, ... array2];
+}
+let array1 = [1, 2];
+let array2 = [7, 8];
+let arrays = funct(array1, array2);
+console.log(arrays);
 
 // rest
-// function funct(argument1, ... argument2) {
-//     console.log(argument1, argument2);
-// }
-// funct('Hello', 1, false);
+function funct(argument1, ... argument2) {
+    console.log(argument1, argument2);
+}
+funct('Hello', 1, false);
 
-// ?????
-// function name(a, b, c) {
-
-// }
+//
+function averageValue(... theArgs) {
+let total = 0;
+for(arg of theArgs){
+    total += arg;
+}
+return total / theArgs.length;
+}
+console.log(averageValue(2, 2, 2));
 
 // typeOf
-// function myFunc(params) {
-//    return params = typeof params;
-// }
-// console.log(myFunc (false));
+function myFunc(params) {
+   return params = typeof params;
+}
+console.log(myFunc (false));
 
 // 
-// function myFunc(params) {
-//     if (typeof params === 'number') {
-//         console.log('Це число');
-//     }
-//     else if (typeof params === 'string') {
-//         console.log('Це рядок');
-//     }
-//     else {
-//         console.log('Це інший тип');
-//     }
-// }
-// myFunc(Number);
+function myFunc(params) {
+    if (typeof params === 'number') {
+        console.log('Це число');
+    }
+    else if (typeof params === 'string') {
+        console.log('Це рядок');
+    }
+    else {
+        console.log('Це інший тип');
+    }
+}
+myFunc(Number);
 
 // Робота з селекторами
 let id = document.getElementById('attribute');
